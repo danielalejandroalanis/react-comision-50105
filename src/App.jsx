99a6f18a5c996461154af922c08bfc6c71731249
Daniel Alanis <danielalanis@MBP-de-Daniel.fibertel.com.ar>
2023-11-07 21:04:57 -0300

    Se agrega clase 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
 import "./index.css";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import { getAllProducts, getSingleProducts } from "./services/apiProducts";
+
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getAllProducts(10)
+      .then((res) => setProducts(res.data.products))
+      .catch((err) => console.log(err));
+
+    getSingleProducts().then((res) => console.log(res));
+  }, []);
+
   return (
     <div>
       <NavBarComponent />
-      <ItemListContainer greeting="Bienvenidos a DocTops!" />
+      <ItemListContainer products={products} />
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,21 @@
-const ItemListContainer = ({greeting}) => {
-    const greetingStyles = {
-        fontSize: '3rem',
-        fontWeight: '700',
-        textAlign: 'center',
-        color: '#f4f4f4'
-    }
-    return (
-        <div style={greetingStyles}>
-            {greeting}
-        </div>
-    )
-}
+import Card from "react-bootstrap/Card";
+
+const ItemListContainer = ({ products }) => {
+  return (
+    <div style={{ display: "flex", flexWrap: 1, width: "100vw" }}>
+      {products.map((product) => {
+        return (
+          <Card key={product.id} style={{ width: "18rem", margin: 20 }}>
+            <Card.Img variant="top" src={product.thumbnail} />
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>{product.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
 
 export default ItemListContainer;
