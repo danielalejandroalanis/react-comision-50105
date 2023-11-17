8fa4267fb473348da09cf94dc0b05411bfa3c136
Daniel Alanis <danielalanis@MBP-de-Daniel.fibertel.com.ar>
2023-11-17 20:32:48 -0300

    Se sube segunda preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import MainRoutes from "./routes/MainRouter";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
 import "./index.css";
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
 function App() {
   return (
     <div>
-      <NavBarComponent />
-      <ItemListContainer greeting="Bienvenidos a DocTops!" />
+      <MainRoutes />
     </div>
   );
 }
